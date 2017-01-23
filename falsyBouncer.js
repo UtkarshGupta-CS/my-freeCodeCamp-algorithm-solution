dc2273199afabfe5baff1ac32a523743ac14c596
UtkarshGupta-CS <utgupta1997@gmail.com>
2017-01-23 10:54:36 +0530

    little change

--- falsyBouncer.js
+++ falsyBouncer.js
@@ -1,13 +1,12 @@
+
 function bouncer(arr) {
 
   var falsy=[false,null,0,"",undefined,NaN];
 
-  function falsyValues(value){
-    if(value!==falsy){
-      return value;
-    }else return false;
-  }
-  return arr.filter(falsyValues);
+  return arr.filter(function(value){
+     if(value!==falsy){return value;}
+     else return false;
+  });
 }
 
 bouncer([7, "ate", "", false, 9]);
