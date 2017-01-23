57a2b479e50ecf6814f929d930c227697e3ff5da
UtkarshGupta-CS <utgupta1997@gmail.com>
2017-01-23 19:01:39 +0530

    Some changes

--- falsyBouncer.js
+++ falsyBouncer.js
@@ -1,12 +1,13 @@
-
 function bouncer(arr) {
 
-  var falsy=[false,null,0,"",undefined,NaN];
+  var falsy = [false, null, 0, "", undefined, NaN];
 
-  return arr.filter(function(value){
-     if(value!==falsy){return value;}
-     else return false;
+  var filtered = arr.filter(function (value) {
+    if (value !== falsy) {
+      return value;
+    } else return false;
   });
+  console.log(filtered);
 }
-
 bouncer([7, "ate", "", false, 9]);
+
