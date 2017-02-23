4247f439c840f39679329d4aacaea76a69bec171
UtkarshGupta-CS <utgupta1997@gmail.com>
2017-02-23 22:56:17 +0530

    Intemediate start

---
+++ Intermediate-Algorithm-Scripting/Sum All Numbers in a Range.js
@@ -0,0 +1,13 @@
+'esversion:6';
+
+function sumAll(arr) {
+var min = Math.min(...arr);
+var max = Math.max(...arr);
+var sum = 0;
+for(var i=min;i<=max;i++){
+  sum +=i;
+}
+  return sum;
+}
+
+sumAll([1, 4]);
